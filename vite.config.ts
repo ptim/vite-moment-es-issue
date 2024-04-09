4ef8d0ffef778ac619124cbaf82fe619c473b9a5
ptim <tim@memelab.com.au>
2024-04-09 12:27:54 +1000

    build: chunk moment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,20 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          console.log(id);
+          if (id.includes('node_modules/moment')) {
+            return 'moment';
+          } else {
+            return 'index';
+          }
+        },
+      },
+    },
+  },
 })
