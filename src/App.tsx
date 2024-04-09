6ae089c12aee2a09ad8ac43b3c5bb6280be95de6
ptim <tim@memelab.com.au>
2024-04-09 12:29:25 +1000

    refactor: sync imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
+import moment from './moment'
 
 function App() {
   const [now, setNow] = React.useState<string>()
 
   React.useEffect(() => {
-    import('./moment').then((m) => {
-      setNow(m.default().format('llll'))
-    })
+    // import('./moment').then((m) => {
+    //   setNow(m.default().format('llll'))
+    // })
+    setNow(moment().format('llll'))
   }, [])
 
   return <p>{now}</p>
